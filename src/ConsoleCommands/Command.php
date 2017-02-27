599ed8d6b31d8b94a50a000f31e9a937e4e55332
grigorecezar <grigorecezar@gmail.com>
2017-02-27 15:46:15 +0200

    abstracted symphony command through our own super class

---
+++ src/ConsoleCommands/Command.php
@@ -0,0 +1,8 @@
+<?php namespace IndexIO\Operatur\ConsoleCommands;
+
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Command extends SymfonyCommand
+{
+}
--- src/ConsoleCommands/GenerateFunctions.php
+++ src/ConsoleCommands/GenerateFunctions.php
@@ -1,6 +1,5 @@
 <?php namespace IndexIO\Operatur\ConsoleCommands;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
