b175f67ce72580f725d871b226382ce8e3eadec6
grigorecezar <grigorecezar@gmail.com>
2017-03-13 15:23:05 +0200

    change default-driver to defautl for consistency across laravel ecosystem

--- src/Config.php
+++ src/Config.php
@@ -11,7 +11,7 @@ class Config
 		$this->sanityCheck($config);
 
 		$this->config = $config;
-		$this->driver = $config['default-driver'];
+		$this->driver = $config['default'];
 		$this->connection = $config['connections'][$this->driver];
 	}
 
