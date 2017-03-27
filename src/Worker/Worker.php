38f137005711880ba700f0d3bcc9cc3ad6462a66
grigorecezar <grigorecezar@gmail.com>
2017-03-27 16:58:08 +0300

    worker declaration including operatur queue

--- src/Worker/Worker.php
+++ src/Worker/Worker.php
@@ -4,18 +4,22 @@ use IndexIO\Operatur\Contracts\Queue as QueueContract;
 use IndexIO\Operatur\Contracts\Worker as WorkerContract;
 use IndexIO\Operatur\Router\Request;
 use IndexIO\Operatur\Router\Route;
+use IndexIO\Operatur\Queue\Queue as Operatur;
 
 abstract class Worker implements WorkerContract
 {
 	protected $request = null;
 	protected $queue = null;
+	protected $operatur = null;
 
-	public function __construct(array $data)
+	public function __construct(array $data, Operatur $operatur = null)
 	{
 		$this->request = new Request(
 			new Route(static::NAME, 'handle'),
 			$data
 		);
+
+		$this->operatur = $operatur;
 	}
 
 	abstract public function handle();
